28452f5ba09162c9352449f919d5c32edb189be7
sjerzak <slawomir.jerzak@gmail.com>
2021-09-27 20:40:19 +0200

    fix http.request

--- controllers/authController.js
+++ controllers/authController.js
@@ -79,21 +79,20 @@ exports.logout = (req, res) => {
 exports.protect = catchAsync(async (req, res, next) => {
   // 1) Getting token and check of it's there
   let token;
-  // if (
-  //   req.headers.authorization &&
-  //   req.headers.authorization.startsWith('Bearer')
-  // ) {
-  console.log(req);
-  // token = req.headers.authorization.split(' ')[1];
-  // } else if (req.cookies.jwt) {
-  //   token = req.cookies.jwt;
-  // }
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  } else if (req.cookies.jwt) {
+    token = req.cookies.jwt;
+  }
 
-  // if (!token) {
-  //   return next(
-  //     new AppError('You are not logged in! Please log in to get access.', 401)
-  //   );
-  // }
+  if (!token) {
+    return next(
+      new AppError('You are not logged in! Please log in to get access.', 401)
+    );
+  }
 
   // 2) Verification token
   const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
