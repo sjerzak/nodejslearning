ed19801da20f9e7f63a95941e3d266b03fa084cc
sjerzak <slawomir.jerzak@gmail.com>
2021-09-27 21:29:30 +0200

    fix http.request

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -1,7 +1,9 @@
 /* eslint-disable */
 import axios from 'axios';
 import { showAlert } from './alerts';
-const stripe = Stripe('pk_test_BUkd0ZXAj6m0q0jMyRgBxNns00PPtgvjjr');
+const stripe = Stripe(
+  'pk_test_51JVbgIFA4p9Ui9SpquCsCf1r8vOQ5sTq3isjE4e3OHsAEHBKTaMFytUNNLld0MSVxMWgyc88NHJrIsFAPUOZUtWO00YI2QQVmf'
+);
 
 export const bookTour = async tourId => {
   try {
