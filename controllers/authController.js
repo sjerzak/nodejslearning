388e25f485b4062b98dd8e65f915f8c8bce348a8
sjerzak <slawomir.jerzak@gmail.com>
2021-09-27 20:29:07 +0200

    fix http.request

--- controllers/authController.js
+++ controllers/authController.js
@@ -83,7 +83,8 @@ exports.protect = catchAsync(async (req, res, next) => {
   //   req.headers.authorization &&
   //   req.headers.authorization.startsWith('Bearer')
   // ) {
-  //   token = req.headers.authorization.split(' ')[1];
+  console.log(req);
+  // token = req.headers.authorization.split(' ')[1];
   // } else if (req.cookies.jwt) {
   //   token = req.cookies.jwt;
   // }
