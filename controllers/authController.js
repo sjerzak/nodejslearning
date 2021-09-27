93052116d29346bcb7446d4a6dffbf7e8f61bb3b
sjerzak <slawomir.jerzak@gmail.com>
2021-09-27 21:08:32 +0200

    fix http.request

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,16 +12,16 @@ const signToken = id => {
   });
 };
 
-const createSendToken = (user, statusCode, res, req) => {
+const createSendToken = (user, statusCode, req, res) => {
   const token = signToken(user._id);
-  const cookieOptions = {
+
+  res.cookie('jwt', token, {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
     secure: req.secure || req.headers['x-forwarded-proto'] === 'https'
-  };
-  res.cookie('jwt', token, cookieOptions);
+  });
 
   // Remove password from output
   user.password = undefined;
